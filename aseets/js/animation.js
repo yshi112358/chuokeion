86e661e1d6f974f549a9699e0e8982b212d587e9
yshi112358 <yshi112358@gmail.com>
2022-05-28 04:19:00 +0900

    コメントの削除

--- aseets/js/animation.js
+++ aseets/js/animation.js
@@ -1,25 +1,3 @@
-/*
-gsap.set('.back-img__1', { autoAlpha: 0 });
-gsap.set('.back-img__2', { autoAlpha: 0 });
-
-gsap.to('.back-img__1', {
-    autoAlpha: 1,
-    scrollTrigger: {
-        trigger: '.back-img__1',
-        start: 'top center',
-        markers: false
-    }
-});
-gsap.to('.back-img__2', {
-    autoAlpha: 1,
-    scrollTrigger: {
-        trigger: '.back-img__2',
-        start: 'top center',
-        markers: false
-    }
-});
-*/
-
 window.addEventListener("load", function () {
     const spinner = document.getElementById('loading');
     spinner.classList.add('loaded');

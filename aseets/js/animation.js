1f0c812c37d10b4d9a167065ea9f10fd8e376832
yshi112358 <yshi112358@gmail.com>
2022-05-28 02:29:59 +0900

    Initial commit

---
+++ aseets/js/animation.js
@@ -0,0 +1,19 @@
+gsap.set('.back-img__1', { autoAlpha: 0 });
+gsap.set('.back-img__2', { autoAlpha: 0 });
+
+gsap.to('.back-img__1', {
+    autoAlpha: 1,
+    scrollTrigger: {
+        trigger: '.back-img__1',
+        start: 'top center',
+        markers: true
+    }
+});
+gsap.to('.back-img__2', {
+    autoAlpha: 1,
+    scrollTrigger: {
+        trigger: '.back-img__2',
+        start: 'top center',
+        markers: true
+    }
+});

4ae58fc7001ac9503ecdeba9bcaae9b8492c0388
yshi112358 <yshi112358@gmail.com>
2022-06-29 00:55:01 +0900

    ScrollTriggerの設定

--- aseets/js/animation.js
+++ aseets/js/animation.js
@@ -3,23 +3,63 @@ window.addEventListener("load", function () {
     spinner.classList.add('loaded');
 });
 
-// const topTl = gsap.timeline({
-//     scrollTrigger: {
-//         trigger: ".background-1",
-//         start: "top top",
-//         end: "+=900",
-//         scrub: 1,
-//         pin: true,
-//         markers: true
-//     },
-// });
-// const topTl2 = gsap.timeline({
-//     scrollTrigger: {
-//         trigger: ".background-2",
-//         start: "top top",
-//         end: "+=900",
-//         scrub: 1,
-//         pin: true,
-//         markers: true
-//     },
-// });
+gsap.timeline({
+    scrollTrigger: {
+        trigger: "#top",
+        start: "top top",
+        end: "+=900",
+        scrub: 1,
+        pin: true,
+        markers: true
+    },
+});
+gsap.timeline({
+    scrollTrigger: {
+        trigger: "#about",
+        start: "top top",
+        end: "+=900",
+        scrub: 1,
+        pin: true,
+        markers: true
+    },
+});
+gsap.timeline({
+    scrollTrigger: {
+        trigger: "#live",
+        start: "top top",
+        end: "+=900",
+        scrub: 1,
+        pin: true,
+        markers: true
+    },
+});
+gsap.timeline({
+    scrollTrigger: {
+        trigger: "#members",
+        start: "top top",
+        end: "+=900",
+        scrub: 1,
+        pin: true,
+        markers: true
+    },
+});
+gsap.timeline({
+    scrollTrigger: {
+        trigger: "#blog",
+        start: "top top",
+        end: "+=900",
+        scrub: 1,
+        pin: true,
+        markers: true
+    },
+});
+gsap.timeline({
+    scrollTrigger: {
+        trigger: "#contact",
+        start: "top top",
+        end: "+=900",
+        scrub: 1,
+        pin: true,
+        markers: true
+    },
+});

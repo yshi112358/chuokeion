013dce09827f43d1fabcdb0ccf9a982ff71ed10a
yshi112358 <yshi112358@gmail.com>
2022-06-28 19:43:54 +0900

    画像の表示方法をimgタグで

--- aseets/js/animation.js
+++ aseets/js/animation.js
@@ -3,23 +3,23 @@ window.addEventListener("load", function () {
     spinner.classList.add('loaded');
 });
 
-const topTl = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".background-1",
-        start: "top top",
-        end: "+=900",
-        scrub: 1,
-        pin: true,
-        markers: true
-    },
-});
-const topTl2 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".background-2",
-        start: "top top",
-        end: "+=900",
-        scrub: 1,
-        pin: true,
-        markers: true
-    },
-});
+// const topTl = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".background-1",
+//         start: "top top",
+//         end: "+=900",
+//         scrub: 1,
+//         pin: true,
+//         markers: true
+//     },
+// });
+// const topTl2 = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".background-2",
+//         start: "top top",
+//         end: "+=900",
+//         scrub: 1,
+//         pin: true,
+//         markers: true
+//     },
+// });
